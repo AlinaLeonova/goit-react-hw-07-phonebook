0e7bc6a165e5e9b3a51d367b1b1c178136a3d643
Alina Leonova <alinaa.leonova@gmail.com>
2020-12-16 16:25:53 +0200

    fix

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,10 +1,8 @@
 import React from "react"
 import styles from "./ContactItem.module.css"
 import PropTypes from 'prop-types'
-import { connect } from "react-redux"
-import contactsOperations from "../../redux/contacts/contactsOperations"
 
-const ContactItem = ({ contact , onRemoveContact}) => {
+const ContactItem = ({ contact, onRemoveContact }) => {
   return (
     <li className={styles.item}>
       <span className={styles.text}>
@@ -21,12 +19,5 @@ ContactItem.propTypes = {
   contact: PropTypes.objectOf(PropTypes.string).isRequired
 }
 
-const mapStateToProps = state=>{
- return state
-};
 
-const mapDispatchToProps = (dispatch, ownProps) => (
-  {onRemoveContact: () => dispatch(contactsOperations.onRemoveContacts(ownProps.contact.id))}
-);
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactItem)
+export default ContactItem
